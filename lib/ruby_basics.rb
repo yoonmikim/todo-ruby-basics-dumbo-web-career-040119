0a3d60e9c7078afe790d5d26c4d45182b9f246cc
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-07 16:04:46 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong!")
+  phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Hi there, "
+      name = "Bobby!"
+  greeting + name
 end
 
-def return_a_value
-
+def return_a_value(value="Nice")
+  value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase="expert")
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(top = "cheese")
+  top
 end
