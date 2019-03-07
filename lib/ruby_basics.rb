58567b6dd545198d86d2fb23bb26047fc78bc285
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-07 16:02:17 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,8 +10,10 @@ def argue(phrase = "I'm right and you are wrong!")
   phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Hi there, "
+      name = "Bobby!"
+  greeting + name
 end
 
 def return_a_value
